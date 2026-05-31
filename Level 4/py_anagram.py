3cd777e9f274677b4c515aac373f6c39539a932a
Felipe Hillebrand <felipehillebrand@gmail.com>
2026-05-31 18:08:53 +0100

    Update py_anagram.py

--- Level 4/py_anagram.py
+++ Level 4/py_anagram.py
@@ -4,7 +4,7 @@ com a mesma quantidade, independentemente de maiúsculas/minúsculas e espaços.
 """
 
 
-def Anagram(s1: str, s2: str) -> bool:
+def anagram(s1: str, s2: str) -> bool:
     new_s1 = s1.upper().replace(" ", "")
     new_s2 = s2.upper().replace(" ", "")
 
@@ -14,13 +14,13 @@ def Anagram(s1: str, s2: str) -> bool:
     return sorted(new_s1) == sorted(new_s2)
 
 
-print(Anagram("listen", "silent")) # True
-print(Anagram("Triangle", "Integral")) # True
-print(Anagram("Dormitory", "Dirty Room")) # True
-print(Anagram("Astronomer", "Moon starer")) # True
-print(Anagram("hello", "world")) # False
-print(Anagram("test", "ttew")) # False
-print(Anagram("abc", "abcc")) # False
-print(Anagram("", "")) # True
-print(Anagram("a gentleman", "elegant man")) # True
-print(Anagram("aabb", "ab")) # False
+print(anagram("listen", "silent")) # True
+print(anagram("Triangle", "Integral")) # True
+print(anagram("Dormitory", "Dirty Room")) # True
+print(anagram("Astronomer", "Moon starer")) # True
+print(anagram("hello", "world")) # False
+print(anagram("test", "ttew")) # False
+print(anagram("abc", "abcc")) # False
+print(anagram("", "")) # True
+print(anagram("a gentleman", "elegant man")) # True
+print(anagram("aabb", "ab")) # False
