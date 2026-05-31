06f22ae9e8c05c32cef076c13eaf21fc1ae6a899
Felipe Hillebrand <felipehillebrand@gmail.com>
2026-05-31 16:12:25 +0100

    Update mirror_matrix.py

--- Level 2/mirror_matrix.py
+++ Level 2/mirror_matrix.py
@@ -2,9 +2,8 @@ def mirror_matrix(matrix: list[list[int]]) -> list[list[int]]:
     return [e[::-1] for e in matrix]
 
 
-input = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-]
-print(mirror_matrix(input))
+print(mirror_matrix([[1, 2, 3], [4, 5, 6]]))       # [[3, 2, 1], [6, 5, 4]]
+print(mirror_matrix([[1, 2], [3, 4], [5, 6]]))      # [[2, 1], [4, 3], [6, 5]]
+print(mirror_matrix([[7]]))                          # [[7]]
+print(mirror_matrix([[1, 2, 3, 4]]))                 # [[4, 3, 2, 1]]
+print(mirror_matrix([[-1, -2], [-3, -4]]))           # [[-2, -1], [-4, -3]]
