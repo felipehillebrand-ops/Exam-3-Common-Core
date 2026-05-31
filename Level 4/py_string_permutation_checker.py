3efb70842ef0a9114e4b93d9fa746a789c05f23c
Felipe Hillebrand <felipehillebrand@gmail.com>
2026-05-31 16:03:10 +0100

    Update py_string_permutation_checker.py

--- Level 4/py_string_permutation_checker.py
+++ Level 4/py_string_permutation_checker.py
@@ -1,3 +1,14 @@
+# def string_permutation_checker(s1: str, s2:str) -> bool
+
+# write a function that determines if two strings are permutation of each other. Two strings are permutation of each other if they contain the same characters with the same frequencies .
+
+# Check if both strings contain the same characters 
+# Count the character frequencies (case sensitive)
+# return True if they are permutations and False otherwise
+# Handle empty strings (they are permutations)
+# Consider whitespaces and punctions as regular charactares
+
+
 def string_permutation_checker(s1: str, s2: str) -> bool:
     return sorted(s1) == sorted(s2)
 
